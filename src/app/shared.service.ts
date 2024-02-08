2dd2f0fff2866c2ed242905db7a9b14a6b3dbb24
Laiju Thomas <laiju@fayausa.com>
2024-02-08 11:13:02 +0530

    feat(core): add fraud detection code

---
+++ src/app/shared.service.ts
@@ -0,0 +1,17 @@
+// ip-geolocation.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedService {
+  private apiUrl = 'https://api.ipgeolocationapi.com/geolocate';
+
+  constructor(private http: HttpClient) {}
+
+  getIpAndLocation(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
